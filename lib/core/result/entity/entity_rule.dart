67e2c27dbef9c13c9883be880e4ddbfa61ee1e42
alifgiant <muh.alifgiant@gmail.com>
2020-03-28 20:02:31 +0700

    rule book

---
+++ lib/core/result/entity/entity_rule.dart
@@ -0,0 +1,6 @@
+class Rule {
+  final String content;
+  final bool isBold;
+
+  Rule(this.content, this.isBold);
+}
--- lib/feature/register/rulebook_screen.dart
+++ lib/feature/register/rulebook_screen.dart
@@ -1,95 +1,113 @@
 import 'package:dirumahaja/core/res/app_color.dart';
-import 'package:flutter/gestures.dart';
+import 'package:dirumahaja/core/result/entity/entity_rule.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_color/flutter_color.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class RuleBookScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: Column(
-        children: <Widget>[
-          Container(height: 64),
-          Text(
-            'Aturan Main',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.raleway(
-              fontSize: 36,
-              fontWeight: FontWeight.w800,
-              color: AppColor.titleColor.toHexColor(),
-            ),
-          ),
-          Container(height: 32),
-          getRuleBox(),
-        ],
-      ),
-    );
-  }
-
-  List<String> getRules() => [
-        '1. Pastikan Koneksi GPS Tersedia',
-        '2. Pastikan Koneksi Internet Lancar',
-        '3. Jangan keluar dari lingkungan rumah (500 m dari titik rumah)',
-        '4. Buat persiapan sebelum memulai tantangan (makanan, minum, obat, dll)',
-        '5. Kamu akan memulai dengan 1 energi ',
-        '6. Energi mu akan berkurang jika tidak memenuhi poin 1,2,3',
-        '7. Energi mu bisa bertambah jika teman mu kehabisan Energi',
-        '8. Jika kamu kehabisan Energi, kamu harus menjalankan hukuman dan akan memulai hitungan hari dari 0',
-        ' ',
-      ];
-
-  Widget getRuleBox() {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 46),
+    return SingleChildScrollView(
       child: Container(
-        height: 420,
-        padding: const EdgeInsets.all(26),
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ...getRules()
-                .map((f) => Text(f,
-                    style: GoogleFonts.muli(
-                      color: AppColor.bodyColor.toHexColor(),
-                    )))
-                .toList(),
-            getPrivacyRow()
+          children: <Widget>[
+            Container(height: 64),
+            Text(
+              'Aturan Main',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.raleway(
+                fontSize: 28,
+                fontWeight: FontWeight.w800,
+                color: AppColor.titleColor.toHexColor(),
+              ),
+            ),
+            Container(height: 8),
+            Text(
+              'Baca dulu biar paham, malu bertanya sesat di jalan',
+              style: GoogleFonts.muli(
+                fontSize: 12,
+                color: AppColor.bodyColor.toHexColor(),
+              ),
+            ),
+            Container(height: 32),
+            getRuleBox(),
           ],
         ),
       ),
     );
   }
 
-  RichText getPrivacyRow() {
-    return RichText(
-      text: TextSpan(
-        text: 'baca  ',
-        style: GoogleFonts.muli(color: AppColor.bodyColor.toHexColor()),
-        children: [
-          getPrivacyPolicy(),
-          TextSpan(
-            text: ' Lebih Lanjut',
-            style: GoogleFonts.muli(color: AppColor.bodyColor.toHexColor()),
-          ),
-        ],
+  List<Rule> getRules() {
+    return [
+      Rule('Pastikan Koneksi GPS Tersedia', true),
+      Rule('Pastikan Koneksi Internet Lancar', true),
+      Rule(
+        'Jangan keluar dari lingkungan rumah \n(500 m dari titik rumah)',
+        true,
+      ),
+      Rule(
+        'Buat persiapan sebelum memulai tantangan (makanan, minum, obat, dll)',
+        false,
+      ),
+      Rule(
+        'Kamu akan memulai dengan 1 energi ',
+        false,
+      ),
+      Rule(
+        'Energi mu akan berkurang jika tidak memenuhi poin 1,2,3',
+        false,
+      ),
+      Rule(
+        'Energi mu bisa bertambah jika teman mu kehabisan Energi',
+        false,
+      ),
+      Rule(
+        'Jika kamu kehabisan energi, kamu harus menjalankan hukuman dan akan memulai hitungan hari dari 0',
+        false,
+      ),
+    ];
+  }
+
+  Widget getRuleBox() {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Container(
+        height: 400,
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: getRules().map((r) => createItem(r)).toList(),
+        ),
       ),
     );
   }
 
-  TextSpan getPrivacyPolicy() {
-    return TextSpan(
-      text: 'privacy policy',
-      style: GoogleFonts.muli(color: HexColor('E96666')),
-      recognizer: new TapGestureRecognizer()
-        ..onTap = () {
-          print('click');
-          launch(
-              'https://www.notion.so/buahbatu/Privacy-Policy-eabfe866c14a43ea895d89d608843bfb');
-        },
+  Row createItem(Rule rule) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          height: 10,
+          width: 10,
+          margin: const EdgeInsets.only(top: 4, right: 8),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: HexColor('F9C126'),
+          ),
+        ),
+        Expanded(
+          child: Text(
+            rule.content,
+            style: GoogleFonts.muli(
+              color: Colors.black87,
+              fontWeight: rule.isBold ? FontWeight.w800 : null,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
